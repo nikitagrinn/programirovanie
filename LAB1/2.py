ba1da128187cee983141569f6ceada04f72ad79f
Никита <nikitagrinenko2003@gmail.com>
2025-09-16 11:37:48 +0300

    Update 2.py

--- LAB1/2.py
+++ LAB1/2.py
@@ -1,12 +1,20 @@
 import unittest
 import first
 
-class TestMySolution(unittest.TestCase):
+# Класс TwoSum для тестирования моего решения
+class TwoSum(unittest.TestCase):
 
-    def test_same_lst_elems(self):
-        self.assertEqual(first.Solution().two_sum([3, 8, 12, 17], 11), (0, 1))
-        self.assertEqual(first.Solution().two_sum([5, 5, 5, 5], 10), (0, 1))
-        self.assertEqual(first.Solution().two_sum([10, 20, 30, 40], 70), (2, 3))
+    def test_positive_numbers(self):
+        # Проверка на простом массиве с положительными числами
+        self.assertEqual(first.Solution().two_sum([1, 6, 8, 14], 20), (1, 3))
+
+    def test_with_duplicates(self):
+        # Проверка, когда в массиве есть одинаковые числа
+        self.assertEqual(first.Solution().two_sum([4, 11, 15, 4], 8), (0, 3))
+
+    def test_end_of_list(self):
+        # Проверка, когда нужная пара находится в конце списка
+        self.assertEqual(first.Solution().two_sum([100, 200, 25, 50], 75), (2, 3))
 
 if __name__ == '__main__':
     unittest.main()
