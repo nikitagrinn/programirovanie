7dd9bcc54e7ac86af06a3524886f52ece1805a48
Никита <nikitagrinenko2003@gmail.com>
2025-09-16 11:38:49 +0300

    Update 1.py

--- LAB1/1.py
+++ LAB1/1.py
@@ -1,21 +1,30 @@
-arr = [5, 12, 18, 21]
-i = 23
+# Задаем мой массив и итоговую сумму
+numbers = [4, 8, 15, 16, 23, 42]
+target_sum = 31
 
-
-class Solution:
-    '''Returns solutions'''
+class TwoSum:
+    '''
+    Этот класс находит пару чисел в списке,
+    сумма которых равна заданному значению.
+    '''
 
     def two_sum(self, lst, target):
-        '''Returns two_sum\'s solution'''
-
+        # Проходим по каждому элементу списка
         for x in range(len(lst)):
+            # Создаем временный список из оставшихся элементов
             temp = lst[x + 1:]
+            # Ищем второе число для пары
             if target - lst[x] in temp:
+                # Если нашли, возвращаем индексы обоих чисел
                 return (x, temp.index(target - lst[x]) + x + 1)
 
+# Создаем экземпляр моего класса
+my_solver = TwoSum()
 
-my_solution = Solution()
-res = my_solution.two_sum([10, 20, 30, 40], 50)
-print(res)
+# Первый вызов с новыми данными
+result1 = my_solver.two_sum([3, 9, 11, 17], 20)
+print(f"Результат для первого вызова: {result1}")
 
-print(my_solution.two_sum(arr, i))
+# Второй вызов с использованием переменных
+result2 = my_solver.two_sum(numbers, target_sum)
+print(f"Результат для второго вызова: {result2}")
